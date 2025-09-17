8eed74b615f5e32f986454b4827e0948e887879d
gat0sy <cheikhmt04@gmail.com>
2025-09-17 21:46:50 +0000

    cleaning a bit the file for future developpement

--- test_model.py
+++ test_model.py
@@ -1,5 +1,4 @@
+###########- currently working on this file
 from model import add_link, get_links, edit_link, delete_link, list_groups , count_in_group
 
-##print(count_in_group("Trava"))
-##add_link("Asphalte Legends - reddit", "https://www.reddit.com/r/Asphalt9/", details="The offcial asphalte sub reddit", group="Gaming")
-print(get_links())
+command = input("""Type one of the options bellow \n-add\n-edit\n-delete\n-display""")
